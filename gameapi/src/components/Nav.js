34161df7c61b1b3c0ed15e682125c2c8f68a1017
Kacper Zawojski <zawojweb@gmail.com>
2021-07-08 20:28:44 +0200

    Update Nav.js

--- gameapi/src/components/Nav.js
+++ gameapi/src/components/Nav.js
@@ -49,7 +49,13 @@ text-align: center;
         outline: none;
     }
     button{
-        
+        font-size: 1.5rem;
+        border: none;
+        padding: 0.5rem 2rem;
+        margin: 1rem;
+        cursor: pointer;
+        background: #ff7675;
+        color: white;
     }
 
 `
